fef664f7c811d6e383a9b267ecedbc492c1622be
hoangluccos <94107943+hoangluccos@users.noreply.github.com>
2024-07-24 12:02:30 +0700

    Language

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -4,12 +4,30 @@ import MenuItem from './MenuItem';
 
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+import Header from './Header';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Menu({ children, items = [] }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
     const renderItems = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        }
+                    }}
+                />
+            );
+        });
     };
     return (
         <Tippy
@@ -18,7 +36,17 @@ function Menu({ children, items = [] }) {
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('content')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title={'Language'}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
         >
